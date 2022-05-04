3c0aa351012ae71191bc7aa7842c3990e94e5939
Eric <eric@haberkorn.co>
2022-05-04 16:12:23 -0400

    Implement delete and upsert events

--- lambda-registrator/delete_event.go
+++ lambda-registrator/delete_event.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"github.com/hashicorp/consul/api"
+)
+
 type DeleteEvent struct {
 	ServiceName    string
 	EnterpriseMeta *EnterpriseMeta
@@ -10,6 +14,40 @@ func (e DeleteEvent) Identifier() string {
 }
 
 func (e DeleteEvent) Reconcile(env Environment) error {
-	env.Logger.Info("Deleting Lambda", "service-name", e.ServiceName)
-	return nil
+	env.Logger.Info("Deleting Lambda service from Consul", "service-name", e.ServiceName)
+
+	env.Logger.Debug("Deleting service defaults config entry", "service-name", e.ServiceName)
+	err := env.deleteServiceDefaults(e)
+	if err != nil {
+		return err
+	}
+
+	env.Logger.Debug("Deregistering service", "service-name", e.ServiceName)
+	return env.deregisterService(e)
+}
+
+func (env Environment) deregisterService(event DeleteEvent) error {
+	deregistration := &api.CatalogDeregistration{
+		Node:      env.NodeName,
+		ServiceID: event.ServiceName,
+	}
+	_, err := env.ConsulClient.Catalog().Deregister(deregistration, event.writeOptions())
+	return err
+}
+
+func (env Environment) deleteServiceDefaults(event DeleteEvent) error {
+	_, err := env.ConsulClient.ConfigEntries().Delete(api.ServiceDefaults, event.ServiceName, event.writeOptions())
+	return err
+}
+
+func (e DeleteEvent) writeOptions() *api.WriteOptions {
+	var writeOptions *api.WriteOptions
+	if e.EnterpriseMeta != nil {
+		writeOptions = &api.WriteOptions{
+			Partition: e.EnterpriseMeta.Partition,
+			Namespace: e.EnterpriseMeta.Namespace,
+		}
+	}
+
+	return writeOptions
 }
--- lambda-registrator/upsert_event.go
+++ lambda-registrator/upsert_event.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"strconv"
+
+	"github.com/hashicorp/consul/api"
+)
+
+const tag = "managed-by-lambda-registrator"
+
 type UpsertEvent struct {
 	CreateService      bool
 	PayloadPassthrough bool
@@ -13,6 +21,72 @@ func (e UpsertEvent) Identifier() string {
 }
 
 func (e UpsertEvent) Reconcile(env Environment) error {
+	if !e.CreateService {
+		return e.toDeleteEvent().Reconcile(env)
+	}
+
 	env.Logger.Info("Upserting Lambda", "arn", e.ARN)
-	return nil
+	env.Logger.Debug("Storing service defaults config entry", "arn", e.ARN)
+	err := env.storeServiceDefaults(e)
+	if err != nil {
+		return err
+	}
+
+	env.Logger.Debug("Registering service", "arn", e.ARN)
+	return env.registerService(e)
+}
+
+func (env Environment) registerService(e UpsertEvent) error {
+	registration := &api.CatalogRegistration{
+		Node:           env.NodeName,
+		SkipNodeUpdate: true,
+		NodeMeta: map[string]string{
+			"external-node":  "true",
+			"external-probe": "true",
+		},
+		Service: &api.AgentService{
+			ID:      e.ServiceName,
+			Service: e.ServiceName,
+			Tags:    []string{tag},
+		},
+	}
+
+	_, err := env.ConsulClient.Catalog().Register(registration, e.writeOptions())
+	return err
+}
+
+func (env Environment) storeServiceDefaults(e UpsertEvent) error {
+	serviceDefaults := &api.ServiceConfigEntry{
+		Kind:     api.ServiceDefaults,
+		Name:     e.ServiceName,
+		Protocol: "http",
+		Meta: map[string]string{
+			enabledTag:            "true",
+			arnTag:                e.ARN,
+			regionTag:             env.Region,
+			payloadPassthroughTag: strconv.FormatBool(e.PayloadPassthrough),
+		},
+	}
+
+	// There is no need for CAS because we are completely regenerating the service
+	// defaults config entry.
+	_, _, err := env.ConsulClient.ConfigEntries().Set(serviceDefaults, e.writeOptions())
+
+	return err
+}
+
+func (e UpsertEvent) toDeleteEvent() DeleteEvent {
+	return DeleteEvent{ServiceName: e.ServiceName, EnterpriseMeta: e.EnterpriseMeta}
+}
+
+func (e UpsertEvent) writeOptions() *api.WriteOptions {
+	var writeOptions *api.WriteOptions
+	if e.EnterpriseMeta != nil {
+		writeOptions = &api.WriteOptions{
+			Partition: e.EnterpriseMeta.Partition,
+			Namespace: e.EnterpriseMeta.Namespace,
+		}
+	}
+
+	return writeOptions
 }
