fdbe55ac06b657311f086972e866d64469c8327e
Chris Thain <chris.m.thain@gmail.com>
2022-10-03 15:41:11 -0700

    update pre-release version for beta2

--- consul-lambda/build-scripts/version.sh
+++ consul-lambda/build-scripts/version.sh
@@ -2,7 +2,7 @@
 
 git_commit=$(git rev-parse --short HEAD)
 version="0.1.0"
-prerelease="beta1"
+prerelease="beta2"
 
 if [ "$prerelease" == "dev" ]; then
     echo "${version}-${prerelease}-${git_commit}"
